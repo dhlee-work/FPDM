f3598fd2875a1eca38210ac0c5a252165638154f
RecordK <RecordK@github.com>
2024-05-20 14:42:54 +0900

    cleancode

--- B1.train_Fusion_model.py
+++ B1.train_Fusion_model.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import os
 from datetime import datetime
-
+from omegaconf import OmegaConf
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import WandbLogger
@@ -47,62 +47,33 @@ def load_logger(args):
     return logger, ckpt_cb
 
 
-def get_parser():
-    def str2bool(v):
-        if isinstance(v, bool):
-            return v
-        if v.lower() in ("yes", "true", "t", "y", "1"):
-            return True
-        elif v.lower() in ("no", "false", "f", "n", "0"):
-            return False
-        else:
-            raise argparse.ArgumentTypeError("Boolean value expected.")
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
 
+
+def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--project_name", type=str, default='deepfashion-fusion-CLIP-patch-learning-sch',
-                        help='Path to config file')
-    parser.add_argument("--root_path", type=str, default='./dataset/deepfashion/', help='Path to config file')
-    parser.add_argument("--phase", type=str, default='train', help='train/test')
-    parser.add_argument("--disable_logger", type=str2bool, default='false')
-    parser.add_argument("--mode", default='client')
-    parser.add_argument("--port", default=52162)
+    parser.add_argument("--config", type=str, default='./config/b1.fpdm.yaml', help='Path to config file')
     return parser.parse_args()
 
 
-args = get_parser()
-args.batch_size = 96
-args.num_workers = 8
-args.combiner_hidden_dim = 512  # large 768 base 512
-args.lr = 0
-args.scheduler_t0 = 10
-args.scheduler_t_mult = 2
-args.scheduler_eta_max = 0.0001
-args.scheduler_t_up = 3
-args.scheduler_gamma = 0.5
-args.temperature = 0.07  # 0.07
-args.weight_decay = 1e-2
-args.max_epochs = 60
-args.scale_size = (256, 256)
-args.lambda_l1 = 0.0001
-args.encoder_type = 'clip'
-args.attn_hidden_dim = 768  # large-1024 base 768
-args.mh_attn_size = 16
-args.img_encoder_update = True
-args.trained_model_name = None  #
-args.wandb_id = None  # '.....'
-args.train_dataset_name = 'train_pairs_data.json'
-args.test_dataset_name = 'test_pairs_data.json'
-args.img_encoder_path = 'openai/clip-vit-base-patch16'
-args.train_patch_embeddings = True
-args.train_patch_embeddings_sampling_ratio = 0.01
 
+args = get_parser()
+config = OmegaConf.load(args.config)
 # if args.trained_model_name:
 #     run_id = args.trained_model_name.split('-')[-1]
 #     wandb.init(id=run_id, resume="allow")
 
-logger, ckpt_cb = load_logger(args)
-train_dataset = read_dataset(args.root_path, args.train_dataset_name)
-test_dataset = read_dataset(args.root_path, args.test_dataset_name)
+logger, ckpt_cb = load_logger(config)
+train_dataset = read_dataset(config.root_path, config.train_dataset_name)
+test_dataset = read_dataset(config.root_path, config.test_dataset_name)
 len(train_dataset)
 
 dat = []
@@ -111,18 +82,18 @@ for i in train_dataset:
     dat.append(os.path.split(i['target_image'])[0])
 dat = list(set(dat))
 
-train_dataset = FusionDataset(train_dataset, args)
+train_dataset = FusionDataset(train_dataset, config)
 train_dataloader = DataLoader(train_dataset,
-                              num_workers=args.num_workers,
-                              batch_size=args.batch_size,
+                              num_workers=config.num_workers,
+                              batch_size=config.batch_size,
                               shuffle=True,
                               drop_last=True,
                               pin_memory=True)
 
-test_dataset = FusionDataset(test_dataset, args)
+test_dataset = FusionDataset(test_dataset, config)
 test_dataloader = DataLoader(test_dataset,
-                             num_workers=args.num_workers,
-                             batch_size=args.batch_size,
+                             num_workers=config.num_workers,
+                             batch_size=config.batch_size,
                              shuffle=True,
                              drop_last=True,
                              pin_memory=True)
@@ -139,10 +110,10 @@ trainer = pl.Trainer(
 
 # Setting the seed
 pl.seed_everything(7)
-if args.trained_model_name:
-    ckpt_path = f'./logs/{args.project_name}/{args.trained_model_name}/last.ckpt'
-    model = FusionModel(args)
+if config.trained_model_name:
+    ckpt_path = f'./logs/{config.project_name}/{config.trained_model_name}/last.ckpt'
+    model = FusionModel(config)
     trainer.fit(model, train_dataloader, test_dataloader, ckpt_path=ckpt_path)
 else:
-    model = FusionModel(args)
+    model = FusionModel(config)
     trainer.fit(model, train_dataloader, test_dataloader)
